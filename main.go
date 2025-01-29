bc3e0f18c92c4917ff2007a56d88ac78fa5408d8
RaihanMalay21 <raihanmalay14@gmail.com>
2025-01-29 02:49:05 -0800

    update command docker in yml

--- main.go
+++ main.go
@@ -34,7 +34,7 @@ func main() {
 	// 	handlers.AllowCredentials(),
 	// )
 
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(":4040", r))
 }
 
 // func corsMiddlewares(next http.Handler) http.Handler {
